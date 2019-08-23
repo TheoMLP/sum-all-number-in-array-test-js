69b1eb745263a2df47e5ee3af187b0cebe63f5fc
TheoMLP <theomalaper.cognez@gmail.com>
2019-08-23 18:47:29 +0000

    finished function

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-function sumItems(array) {
+const sumItems = function(array) {
   // Sum all the numbers in the array
-}
+  let sum = 0;
+  for (let num of array) {
+    if (Array.isArray(num)) {
+      sum += sumItems(num);
+    } else {
+      sum += num;
+    }
+  }
+  return sum;
+};
 
 module.exports = sumItems;
+
